3fc89a032fee75da5e3633470edf483cbf30bab0
Joshua Nanninga <jmarknanninga@gmail.com>
2025-03-20 09:22:45 -0500

    initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Ambient Sound Generator',
+        short_name: 'AmbientPWA',
+        theme_color: '#1a1a1a',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,mp3,wav}']
+      }
+    })
+  ]
+})
